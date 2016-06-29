05f9d9cef13830346f881bee5e70d5c2b56afb35
graemerocher <graeme.rocher@gmail.com>
2016-06-29 10:43:18 +0200

    Port documentation to MongoDB project

--- travis-publish.sh
+++ travis-publish.sh
@@ -31,6 +31,55 @@ if [[ $TRAVIS_REPO_SLUG == "grails/gorm-mongodb" && $TRAVIS_PULL_REQUEST == 'fal
     echo "Publishing Successful."
   fi
   
+
+  if [[ $EXIT_STATUS -eq 0 ]]; then
+    echo "Publishing Successful."
+
+    echo "Publishing Documentation..."
+    ./gradlew docs:docs
+
+    git config --global user.name "$GIT_NAME"
+    git config --global user.email "$GIT_EMAIL"
+    git config --global credential.helper "store --file=~/.git-credentials"
+    echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials
+
+
+    git clone https://${GH_TOKEN}@github.com/grails/grails-data-mapping.git -b gh-pages gh-pages --single-branch > /dev/null
+    cd gh-pages
+
+    if [[ -n $TRAVIS_TAG ]]; then
+        version="$TRAVIS_TAG"
+        version=${version:1}
+
+         mkdir -p latest/mongodb
+         cp -r ../docs/build/docs/. ./latest/mongodb/
+         git add latest/mongodb/*
+
+        majorVersion=${version:0:4}
+        majorVersion="${majorVersion}x"
+
+        mkdir -p "$version/mongodb"
+        cp -r ../docs/build/docs/. "./$version/mongodb/"
+        git add "$version/mongodb/*"
+
+        mkdir -p "$majorVersion/mongodb"
+        cp -r ../docs/build/docs/. "./$majorVersion/mongodb/"
+        git add "$majorVersion/mongodb/*"
+
+    else
+        # If this is the master branch then update the snapshot
+        mkdir -p snapshot/mongodb
+        cp -r ../docs/build/docs/. ./snapshot/mongodb/
+
+        git add snapshot/mongodb/*
+    fi
+
+
+    git commit -a -m "Updating MongoDB Docs for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
+    git push origin HEAD
+    cd ../../..
+    rm -rf gh-pages
+  fi  
 fi
 
 exit $EXIT_STATUS
