bd26bb4b1883d793fca0a6a6f37bebe1760fc301
Graeme Rocher <graeme.rocher@gmail.com>
2016-07-08 15:44:44 +0200

    Tests for database per tenant in RxGORM for MongoDB

--- grails-datastore-gorm-rx-mongodb/src/test/groovy/grails/gorm/rx/mongodb/multitenancy/SingleTenancySpec.groovy
+++ grails-datastore-gorm-rx-mongodb/src/test/groovy/grails/gorm/rx/mongodb/multitenancy/SingleTenancySpec.groovy
@@ -1,30 +1,29 @@
-package org.grails.datastore.gorm.mongo
+package grails.gorm.rx.mongodb.multitenancy
 
-import grails.gorm.MultiTenant
 import grails.gorm.annotation.Entity
-import grails.mongodb.MongoEntity
+import grails.gorm.rx.MultiTenant
+import grails.gorm.rx.mongodb.RxMongoEntity
 import org.bson.types.ObjectId
-import org.grails.datastore.mapping.core.Session
-import org.grails.datastore.mapping.mongo.MongoDatastore
+import org.grails.datastore.mapping.core.DatastoreUtils
 import org.grails.datastore.mapping.mongo.config.MongoSettings
 import org.grails.datastore.mapping.multitenancy.exceptions.TenantNotFoundException
 import org.grails.datastore.mapping.multitenancy.resolvers.SystemPropertyTenantResolver
+import org.grails.datastore.rx.mongodb.RxMongoDatastoreClient
 import spock.lang.AutoCleanup
 import spock.lang.Shared
 import spock.lang.Specification
 /**
- * Created by graemerocher on 07/07/2016.
+ * Created by graemerocher on 08/07/2016.
  */
 class SingleTenancySpec extends Specification {
-
-    @Shared @AutoCleanup MongoDatastore datastore
+    @Shared @AutoCleanup RxMongoDatastoreClient datastoreClient
 
     void setupSpec() {
         Map config = [
-                "grails.gorm.multiTenancy.mode":"SINGLE",
+                "grails.gorm.multiTenancy.mode"               :"SINGLE",
                 "grails.gorm.multiTenancy.tenantResolverClass":SystemPropertyTenantResolver,
-                (MongoSettings.SETTING_URL): "mongodb://localhost/defaultDb",
-                (MongoSettings.SETTING_CONNECTIONS): [
+                (MongoSettings.SETTING_URL)                   : "mongodb://localhost/defaultDb",
+                (MongoSettings.SETTING_CONNECTIONS)           : [
                         test1: [
                                 url: "mongodb://localhost/test1Db"
                         ],
@@ -33,7 +32,7 @@ class SingleTenancySpec extends Specification {
                         ]
                 ]
         ]
-        this.datastore = new MongoDatastore(config, getDomainClasses() as Class[])
+        this.datastoreClient = new RxMongoDatastoreClient(DatastoreUtils.createPropertyResolver(config), getDomainClasses() as Class[])
     }
 
     void setup() {
@@ -52,56 +51,54 @@ class SingleTenancySpec extends Specification {
         given:
         System.setProperty(SystemPropertyTenantResolver.PROPERTY_NAME, "test1")
         expect:
-        City.DB.name == "defaultDb"
         CompanyB.DB.name == 'test1Db'
     }
 
     void "Test persist and retrieve entities with multi tenancy"() {
         setup:
         CompanyB.eachTenant {
-            CompanyB.DB.drop()
+            DB.drop().toBlocking().first()
         }
 
         when:"A tenant id is present"
         System.setProperty(SystemPropertyTenantResolver.PROPERTY_NAME, "test1")
 
         then:"the correct tenant is used"
-        CompanyB.count() == 0
+        CompanyB.count().toBlocking().first() == 0
         CompanyB.DB.name == 'test1Db'
 
         when:"An object is saved"
-        new CompanyB(name: "Foo").save(flush:true)
+        new CompanyB(name: "Foo").save(flush:true).toBlocking().first()
 
         then:"The results are correct"
-        CompanyB.count() == 1
+        CompanyB.count().toBlocking().first() == 1
 
         when:"The tenant id is switched"
         System.setProperty(SystemPropertyTenantResolver.PROPERTY_NAME, "test2")
 
         then:"the correct tenant is used"
         CompanyB.DB.name == 'test2Db'
-        CompanyB.count() == 0
-        CompanyB.withTenant("test1") { Session s ->
-            CompanyB.count() == 1
+        CompanyB.count().toBlocking().first() == 0
+        CompanyB.withTenant("test1") {
+            count().toBlocking().first() == 1
         }
 
         when:"each tenant is iterated over"
         Map tenantIds = [:]
         CompanyB.eachTenant { String tenantId ->
-            tenantIds.put(tenantId, CompanyB.count())
+            tenantIds.put(tenantId, count().toBlocking().first())
         }
 
         then:"The result is correct"
         tenantIds == [test1:1, test2:0]
     }
-
     List getDomainClasses() {
-        [City, CompanyB]
+        [CompanyB]
     }
 
 }
 @Entity
-class CompanyB implements MongoEntity<CompanyB>, MultiTenant {
+class CompanyB implements RxMongoEntity<CompanyB>, MultiTenant<CompanyB> {
     ObjectId id
     String name
 }
