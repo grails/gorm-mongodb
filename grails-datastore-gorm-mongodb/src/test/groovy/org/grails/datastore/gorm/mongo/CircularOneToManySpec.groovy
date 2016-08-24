e66862e0fc288c4bb86fcf44a61aebb6f16dba6d
Graeme Rocher <graeme.rocher@gmail.com>
2016-08-24 09:25:05 +0200

    Don't cascade deletes for circular single ended associations. Fixes #7

--- grails-datastore-gorm-mongodb/src/test/groovy/org/grails/datastore/gorm/mongo/CircularOneToManySpec.groovy
+++ grails-datastore-gorm-mongodb/src/test/groovy/org/grails/datastore/gorm/mongo/CircularOneToManySpec.groovy
@@ -30,6 +30,24 @@ class CircularOneToManySpec extends GormDatastoreSpec {
 
     }
 
+    @Issue('https://github.com/grails/gorm-mongodb/issues/7')
+    void "Test that deleting a child doesn't not delete the parent in a circular association"() {
+        given:"A circular one-to-many"
+        new Profile(name: "Fred")
+                .addToFriends(name: "Bob")
+                .addToFriends(name: "Frank")
+                .save(flush:true)
+
+        session.clear()
+
+        when:"A child is deleted"
+        Profile.findByName("Bob").delete(flush: true)
+        session.clear()
+
+        then:"The parent wasn't deleted"
+        Profile.count() == 2
+    }
+
     @Override
     List getDomainClasses() {
         [Profile]
