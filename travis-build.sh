073f54538d4772dda013a15a27cced01084f6fc2
Graeme Rocher <graeme.rocher@gmail.com>
2016-08-31 15:30:52 +0200

    Release 6.0.0.RC1

--- travis-build.sh
+++ travis-build.sh
@@ -5,18 +5,24 @@ EXIT_STATUS=0
 
 
 ./gradlew compileTestGroovy
-./gradlew check --refresh-dependencies -no-daemon -x grails2-plugin:test -x gorm-mongodb-spring-boot:test  || EXIT_STATUS=$?
-if [[ $EXIT_STATUS -eq 0 ]]; then
-    ./gradlew grails2-plugin:test --refresh-dependencies -no-daemon || EXIT_STATUS=$?
-fi
-if [[ $EXIT_STATUS -eq 0 ]]; then 
-    ./gradlew gorm-mongodb-spring-boot:test --refresh-dependencies -no-daemon || EXIT_STATUS=$?
-fi
+if [[ $TRAVIS_TAG =~ ^v[[:digit:]] ]]; then
+	echo "Skipping tests to Publish release"
+	./travis-publish.sh || EXIT_STATUS=$?
+else
+	./gradlew check --refresh-dependencies -no-daemon -x grails2-plugin:test -x gorm-mongodb-spring-boot:test  || EXIT_STATUS=$?
+	if [[ $EXIT_STATUS -eq 0 ]]; then
+	    ./gradlew grails2-plugin:test --refresh-dependencies -no-daemon || EXIT_STATUS=$?
+	fi
+	if [[ $EXIT_STATUS -eq 0 ]]; then 
+	    ./gradlew gorm-mongodb-spring-boot:test --refresh-dependencies -no-daemon || EXIT_STATUS=$?
+	fi
 
-./gradlew --stop
+	./gradlew --stop
 
-if [[ $EXIT_STATUS -eq 0 ]]; then
-    ./travis-publish.sh || EXIT_STATUS=$?
+	if [[ $EXIT_STATUS -eq 0 ]]; then
+	    ./travis-publish.sh || EXIT_STATUS=$?
+	fi
+
 fi
 
 exit $EXIT_STATUS
