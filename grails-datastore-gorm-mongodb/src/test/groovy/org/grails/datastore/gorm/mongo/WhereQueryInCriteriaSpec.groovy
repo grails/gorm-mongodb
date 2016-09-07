52f7f67768e6c114d9d26dcc0078923977e0d9a3
James Kleeh <james.kleeh@gmail.com>
2016-09-07 09:44:20 -0400

    Add failing test

--- grails-datastore-gorm-mongodb/src/test/groovy/org/grails/datastore/gorm/mongo/WhereQueryInCriteriaSpec.groovy
+++ grails-datastore-gorm-mongodb/src/test/groovy/org/grails/datastore/gorm/mongo/WhereQueryInCriteriaSpec.groovy
@@ -3,15 +3,17 @@ package org.grails.datastore.gorm.mongo
 import grails.gorm.annotation.Entity
 import grails.gorm.tests.GormDatastoreSpec
 import groovy.transform.NotYetImplemented
+import spock.lang.Shared
 
-/**
- * Created by Jim on 9/5/2016.
- */
 class WhereQueryInCriteriaSpec extends GormDatastoreSpec {
+
+    @Shared
+    Long owner2Id
 
     private void buildTestData() {
         new InCritOwner(name: "Foo 1").addToDogs(name: "Chapter 1").addToDogs(name: "Chapter 2").save(flush: true, failOnError: true)
-        new InCritOwner(name: "Foo 2").addToDogs(name: "Chapter 3").addToDogs(name: "Chapter 4").save(flush: true, failOnError: true)
+        def owner2 = new InCritOwner(name: "Foo 2").addToDogs(name: "Chapter 3").addToDogs(name: "Chapter 4").save(flush: true, failOnError: true)
+        owner2Id = owner2.id
         session.clear()
     }
 
@@ -31,6 +33,20 @@ class WhereQueryInCriteriaSpec extends GormDatastoreSpec {
         given:
         buildTestData()
         def ownerList = [InCritOwner.findByName('Foo 2')]
+        List<InCritDog> dogs = InCritDog.where {
+            owner in ownerList
+        }.list()
+
+        expect:
+        dogs.size() == 2
+        dogs[0].name == 'Chapter 3'
+        dogs[1].name == 'Chapter 4'
+    }
+
+    void "test where query in with list of proxies on right side"() {
+        given:
+        buildTestData()
+        def ownerList = [InCritOwner.load(owner2Id)]
         List<InCritDog> dogs = InCritDog.where {
             owner in ownerList
         }.list()
