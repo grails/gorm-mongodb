731119b3c3e98cfd0f368b208ab268113070fb21
sdelamo <sergio.delamo@softamo.com>
2018-12-10 18:30:58 +0100

    remove —refresh-dependencies

--- travis-build.sh
+++ travis-build.sh
@@ -9,9 +9,9 @@ else
 	./gradlew --no-daemon compileGroovy
 	./gradlew --no-daemon compileTestGroovy
 
-	./gradlew -Dgeb.env=chromeHeadless check --refresh-dependencies -no-daemon -x gorm-mongodb-spring-boot:test  || EXIT_STATUS=$?
+	./gradlew -Dgeb.env=chromeHeadless check -no-daemon -x gorm-mongodb-spring-boot:test  || EXIT_STATUS=$?
 	if [[ $EXIT_STATUS -eq 0 ]]; then 
-	    ./gradlew gorm-mongodb-spring-boot:test --refresh-dependencies -no-daemon || EXIT_STATUS=$?
+	    ./gradlew gorm-mongodb-spring-boot:test -no-daemon || EXIT_STATUS=$?
 	fi
 
 	./gradlew --stop
