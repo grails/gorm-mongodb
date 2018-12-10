6b26d4ce03e9b3350fb31cd3642ce5f72c120f68
sdelamo <sergio.delamo@softamo.com>
2018-12-10 18:30:19 +0100

    add -Dgeb.env=chromeHeadless to travis-build.sh

--- travis-build.sh
+++ travis-build.sh
@@ -9,7 +9,7 @@ else
 	./gradlew --no-daemon compileGroovy
 	./gradlew --no-daemon compileTestGroovy
 
-	./gradlew check --refresh-dependencies -no-daemon -x gorm-mongodb-spring-boot:test  || EXIT_STATUS=$?
+	./gradlew -Dgeb.env=chromeHeadless check --refresh-dependencies -no-daemon -x gorm-mongodb-spring-boot:test  || EXIT_STATUS=$?
 	if [[ $EXIT_STATUS -eq 0 ]]; then 
 	    ./gradlew gorm-mongodb-spring-boot:test --refresh-dependencies -no-daemon || EXIT_STATUS=$?
 	fi
