772b59df18cca5e802380df1bafbb2e671f5874c
sdelamo <sergio.delamo@softamo.com>
2018-12-11 09:53:05 +0100

    Revert "disable rx-mongodb"
    This reverts commit bf46222882a9ca63497ed66fc1a7e4e9fe729614.

--- travis-build.sh
+++ travis-build.sh
@@ -6,10 +6,10 @@ if [[ $TRAVIS_TAG =~ ^v[[:digit:]] ]]; then
 	echo "Skipping tests to Publish release"
 	./travis-publish.sh || EXIT_STATUS=$?
 else
-	./gradlew --no-daemon compileGroovy -x grails-datastore-gorm-rx-mongodb:compileGroovy -x rx-plugin:compileGroovy -x examples-grails3-rxmongodb:compileGroovy
-	./gradlew --no-daemon compileTestGroovy -x grails-datastore-gorm-rx-mongodb:compileTestGroovy -x rx-plugin:compileTestGroovy -x examples-grails3-rxmongodb:compileTestGroovy
+	./gradlew --no-daemon compileGroovy
+	./gradlew --no-daemon compileTestGroovy
 
-	./gradlew -Dgeb.env=chromeHeadless check -no-daemon -x gorm-mongodb-spring-boot:test -x grails-datastore-gorm-rx-mongodb:check -x rx-plugin:check -x examples-grails3-rxmongodb:check || EXIT_STATUS=$?
+	./gradlew -Dgeb.env=chromeHeadless check -no-daemon -x gorm-mongodb-spring-boot:test  || EXIT_STATUS=$?
 	if [[ $EXIT_STATUS -eq 0 ]]; then 
 	    ./gradlew gorm-mongodb-spring-boot:test -no-daemon || EXIT_STATUS=$?
 	fi
