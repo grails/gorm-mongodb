c9f3b0546452911e3d0bbb92a8db36ee5782a6d7
sdelamo <sergio.delamo@softamo.com>
2018-12-11 10:20:22 +0100

    simplify travis build

--- travis-build.sh
+++ travis-build.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 EXIT_STATUS=0
 
-
 if [[ $TRAVIS_TAG =~ ^v[[:digit:]] ]]; then
-	echo "Skipping tests to Publish release"
-	./travis-publish.sh || EXIT_STATUS=$?
+    echo "Skipping tests to Publish release"
+    ./travis-publish.sh || EXIT_STATUS=$?
 else
-	./gradlew --no-daemon compileGroovy
-	./gradlew --no-daemon compileTestGroovy
-
-	./gradlew -Dgeb.env=chromeHeadless check -no-daemon -x gorm-mongodb-spring-boot:test  || EXIT_STATUS=$?
-	if [[ $EXIT_STATUS -eq 0 ]]; then 
-	    ./gradlew gorm-mongodb-spring-boot:test -no-daemon || EXIT_STATUS=$?
-	fi
-
-	if [[ $EXIT_STATUS -eq 0 ]]; then
-	    ./travis-publish.sh || EXIT_STATUS=$?
-	fi
-
+	./gradlew --no-daemon compileGroovy --console=plain || EXIT_STATUS=$?
+    if [ $EXIT_STATUS -ne 0 ]; then
+        exit $EXIT_STATUS
+    fi
+	./gradlew --no-daemon compileTestGroovy --console=plain || EXIT_STATUS=$?
+    if [ $EXIT_STATUS -ne 0 ]; then
+        exit $EXIT_STATUS
+    fi
+    ./gradlew -Dgeb.env=chromeHeadless check --no-daemon --console=plain || EXIT_STATUS=$?
+    if [ $EXIT_STATUS -ne 0 ]; then
+        exit $EXIT_STATUS
+    fi
+    ./travis-publish.sh || EXIT_STATUS=$?
 fi
 
 exit $EXIT_STATUS
