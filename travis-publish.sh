9cad7d1bdcf75fc6b237e713319118febd959528
Graeme Rocher <graeme.rocher@gmail.com>
2018-12-12 16:01:19 +0100

    Prepare build for bintray publishing

--- travis-publish.sh
+++ travis-publish.sh
@@ -12,25 +12,23 @@ if [[ $TRAVIS_REPO_SLUG == "grails/gorm-mongodb" && $TRAVIS_PULL_REQUEST == 'fal
 
   echo "Publishing archives"
   export GRADLE_OPTS="-Xmx1500m -Dfile.encoding=UTF-8"
-  openssl aes-256-cbc -pass pass:$SIGNING_PASSPHRASE -in secring.gpg.enc -out secring.gpg -d
-
-  gpg --keyserver keyserver.ubuntu.com --recv-key $SIGNING_KEY
   if [[ $TRAVIS_TAG =~ ^v[[:digit:]] ]]; then
     # for releases we upload to Bintray and Sonatype OSS
-    ./gradlew -Psigning.keyId="$SIGNING_KEY" -Psigning.password="$SIGNING_PASSPHRASE" -Psigning.secretKeyRingFile="${TRAVIS_BUILD_DIR}/secring.gpg" uploadArchives --no-daemon || EXIT_STATUS=$?
-
-    if [[ $EXIT_STATUS -eq 0 ]]; then
-        ./gradlew -Psigning.keyId="$SIGNING_KEY" -Psigning.password="$SIGNING_PASSPHRASE" -Psigning.secretKeyRingFile="${TRAVIS_BUILD_DIR}/secring.gpg" publish --no-daemon || EXIT_STATUS=$?
-    fi
-
-
-    if [[ $EXIT_STATUS -eq 0 ]]; then
-        ./gradlew -Psigning.keyId="$SIGNING_KEY" -Psigning.password="$SIGNING_PASSPHRASE" -Psigning.secretKeyRingFile="${TRAVIS_BUILD_DIR}/secring.gpg" bintrayUpload --no-daemon || EXIT_STATUS=$?
-    fi
+      if [[ -n $TRAVIS_TAG ]]; then
+          ./gradlew publish bintrayUpload --no-daemon --stacktrace || EXIT_STATUS=$?
+    #     if [[ $EXIT_STATUS -eq 0 ]]; then
+    #       ./gradlew synchronizeWithMavenCentral --no-daemon
+    #     fi
+      else
+          ./gradlew publish --no-daemon --stacktrace || EXIT_STATUS=$?
+      fi
   else
+    echo "publishing snapshot"
     # for snapshots only to repo.grails.org
-    ./gradlew -Psigning.keyId="$SIGNING_KEY" -Psigning.password="$SIGNING_PASSPHRASE" -Psigning.secretKeyRingFile="${TRAVIS_BUILD_DIR}/secring.gpg" publish || EXIT_STATUS=$?
+    ./gradlew publish || EXIT_STATUS=$?
   fi
+
+
   if [[ $EXIT_STATUS -eq 0 ]]; then
     echo "Publishing Successful."
   fi
