ff4c67dfafcba85201c90c3db9b5e94d57b159d9
Puneet Behl <behl4485@gmail.com>
2020-06-02 16:05:19 +0530

    grails/gorm-hibernate5#202 Added tests to TestServiceSpec
    * To verify that autowire by name and type is working

--- examples/test-data-service/src/main/groovy/example/TestBean.groovy
+++ examples/test-data-service/src/main/groovy/example/TestBean.groovy
@@ -1,11 +1,16 @@
 package example
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
 
 class TestBean {
 
     @Autowired
     BookService bookRepo
+
+    @Autowired
+    @Qualifier("bookService")
+    def bookService
 
     void doSomething() {
         assert bookRepo != null
